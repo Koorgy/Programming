ea4f85aa43be3c77494d2b8eb264d3f14f6b22ee
Korgy <Kortoshechka666rus@gmail.com>
2021-03-03 12:32:02 +0300

    C:/Program Files/Git/asd

--- Practice/06/C++/06.cpp
+++ Practice/06/C++/06.cpp
@@ -10,58 +10,44 @@ int main() {
     int a, b, c;
     double D, x1, x2, x;
 
-
+    cout << "Введите a, b, c: "
     cin >> a >> b >> c;
-       
     D = b*b - (4*a*c);
 
     if (D < 0){
+        cout << "D = 0, Так нинадо" << endl;
 
-        cout << "Kak tack?" << endl;
         return 0;
-
     }
 
     if ( a == 0 ) {
-
         x = -c/b;
 
         cout << "x = " << x;
 
         return 0;
     }
-
     else {
-        
-
 
     if (D == 0) {
-
         x = -b/(2*a);
 
         cout << "x =" << x << endl;
 
         return 0;
-
     }
     
     else {
-
         x1 = (-b - sqrt(D)) / (2 * a);
-
         x2 = (-b + sqrt(D)) / (2 * a);
 
-
         if (x1==x2){
-
             cout << "x = " << x1 << endl;
 
             return 0;
-
         }
         else {
         cout << "x1 = " << x1 << endl << "x2 = " << x2 << endl;
-
         }
     }
     }
