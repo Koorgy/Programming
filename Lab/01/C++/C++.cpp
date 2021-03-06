95d86e34aa3f7b0f2820eb47953dbcdbcd2e945a
Korgy <kortoshechka666rus@gmail.com>
2021-03-06 15:19:13 +0300

    1j2ije1

--- Lab/01/C++/C++.cpp
+++ Lab/01/C++/C++.cpp
@@ -1,48 +1,84 @@
 ﻿#include <iostream>
 #include <string>
 #include <nlohmann/json.hpp>
+#include <cpp_httplib/httplib.h>
+using namespace httplib;
 using json = nlohmann::json;
 using namespace std;
+Client cli("http://worldtimeapi.org");
 
-int main()
-{
-    // Какой-то JSON в виде строки
-    string str =
-        R"({
-  "pi": 3.141,
-  "happy": true,
-  "name": "Niels",
-  "nothing": null,
-  "answer": {
-    "everything": 42
-  },
-  "list": [1, 0, 2],
-  "object": {
-    "currency": "USD",
-    "value": 42.99
-  }
-})";
-    // Парсим строку и получаем объект JSON
-    json j = json::parse(str);
-    // Достаём значения
-    double pi = j["pi"];                 cout << "pi " << pi << endl;
-    bool happy = j["happy"];             cout << "happy " << happy << endl;
-    string name = j["name"];             cout << "name " << name << endl;
-    double value = j["object"]["value"]; cout << "value " << value << endl;
-    cout << "every " << to_string(j["answer"]["everything"].get<int>()) << endl;
-    // Идём по массиву
-    for (int i = 0; i < j["list"].size(); i++)
-        cout << "list[" << i << "] = " << j["list"][i] << endl;
-    // Пустой JSON
-    json j2;
-    // Заполняем разными данными
-    j2["num"] = 1;
-    j2["array"] = json::array();
-    j2["array"].push_back(1);
-    j2["array"].push_back(2);
-    j2["object"] = json::object();
-    j2["object"].push_back({ "PI", pi });
-    j2["object"].push_back({ "exp", 2.71 });
-    // Преобразуем в строку и выводим
-    std::cout << j2.dump(4);
+json cache;
+Client times("https://api.openweathermap.org");
+
+json get_json() {
+    auto res = cli.Get("/data/2.5/onecall?lat=44.95719&lon=34.11079&exclude=hourly&appid=efb20d329caaf24c185748f1042f4a17");
+    if (!res) {
+        return ("Err");
+    }
+
+    int status = res->status;
+    if (status < 200 or status >= 300) {
+        return ("Err");
+    }
+
+    return json::parse(res->body);
+}
+
+json get_cache() {
+    json cache;
+    std::ifstream cachename("cache.json");
+    if (cachename.is_open()) {
+        string inform;
+        getline(cachename, inform, '\0');
+
+        if (!inform.empty()) {
+            cache = json::parse(inform);
+        }
+        cachename.close();
+    }
+
+    else {
+        return ("Err");
+    }
+    return cache;
+}
+
+bool cachejson(json ca) {
+    cache = ca;
+    ofstream cachename("cache.json");
+    if (cachename.is_open()) {
+        cachename << ca.dump(4);
+        cachename.close();
+    }
+    else return false;
+    return true;
+}
+
+json get_time() {
+    auto time = times.Get("/api/timezone/Europe/Simferopol");
+    if (!time) {
+        return("Err");
+        return json::object();
+    }
+    int status = time->status;
+    if (status < 200 or status >= 300) {
+        return ("Err");
+    }
+    return json::parse(time->body);
+}
+
+json get_hourly_request(json& hourly) {
+    json hourly_request;
+    int last = hourly.size() - 1;
+    json timenow = get_time();
+    int currtime = timenow["unixtime"];
+    if (hourly[last]["dt"] <= currtime) {
+        return json::object();
+    }
+    for (int i = 0; i <= last; ++i) {
+        if (hourly[i]["dt"] > currtime) {
+            hourly_request = hourly[i];
+            return hourly_request;
+        }
+    }
 }
